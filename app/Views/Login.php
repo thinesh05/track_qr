727e10d513bfe9bcca2454de42b4fbdf19cedaad
shamini-cell <shaminisham2304@gmail.com>
2023-05-31 17:11:48 +0800

    jquery
    - use cdn jquery first

--- app/Views/Login.php
+++ app/Views/Login.php
@@ -38,7 +38,8 @@
 <span class="submit-btn">submit</span>
 
 <!-- SCRIPTS -->
-<script src="<?= base_url('js/jquery/dist/jquery.min.js') ?>"></script>
+<!-- <script src="<?= base_url('js/jquery/dist/jquery.min.js') ?>"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 <script>
     $(".submit-btn").on("click", function(){
