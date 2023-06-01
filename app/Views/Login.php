122c98a1b27f122e4f70df8f11acd5936512fff3
shamini-cell <shaminisham2304@gmail.com>
2023-06-01 13:09:13 +0800

    login page

--- app/Views/Login.php
+++ app/Views/Login.php
@@ -20,22 +20,141 @@
             -moz-osx-font-smoothing: grayscale;
             text-rendering: optimizeLegibility;
         }
+
+        .welcomeFont {
+            font-weight:700;
+            color : #4495FA;
+            font-size:36px;
+        }
+
         .submit-btn {
-            border: 1px solid black;
-            padding: 10px;
+            border: 2px solid #4495FA;
+            color :#4495FA;
+            padding: 10px; 
             cursor: pointer;
+            border-radius : 36px;
+            /* padding-right: 10%;
+            padding-left:10%; */
+            width : 100%;
+            font-weight : 700;
+            margin-top :15px;
+
         }
         .submit-btn:hover {
-            border: 1px solid black;
             color: #fff;
-            background: black;
+            background: #4495FA;
+            border-radius : 36px;
+            width : 100%;
         }
+
+        #username,#password {
+            border: 1px solid transparent;
+            padding: 10px;
+            border-radius : 36px;
+            background: #F8F8F8;
+            width : 100%;
+            font-weight : 100;         
+            margin-top : 15px;   
+        }
+
+        * {
+  box-sizing: border-box;
+}
+
+
+/* style inputs and link buttons */
+input,
+.btn {
+  width: 100%;
+  padding: 12px;
+  border: none;
+  border-radius: 36px;
+  background: #F8F8F8;
+  margin: 5px 0;
+  opacity: 0.85;
+  display: inline-block;
+  font-size: 17px;
+  line-height: 20px;
+  text-decoration: none; /* remove underline from anchors */
+}
+
+input:hover,
+.btn:hover {
+  opacity: 1;
+}
+
+
+/* Two-column layout */
+.col {
+  float: left;
+  width: 50%;
+  margin: auto;
+  padding: 0 50px;
+  margin-top: 6px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+    
+
     </style>
 </head>
+
 <body>
 
-<input type="text" id="name">
-<span class="submit-btn">submit</span>
+
+<div class="container">
+  <form action="/action_page.php">
+    <div class="row">
+
+      <div class="col">
+        <img id="welcomeImg" src="assets/welcomePage.png" alt="Welcome">
+      </div>
+
+      <div class="col">
+        <img id="loginLogo" src="assets/Blue_Modern_QRCode.png" alt="trackQR_Logo">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <input class="submit-btn" type="submit" value="Login">
+      </div>
+      
+    </div>
+  </form>
+</div>
+
+
+<!-- <div class="container">
+  <div class="row-12 flex">
+    <div class="col-6">
+        <img id="welcomeImg" src="assets/welcomePage.png" alt="Welcome">
+    </div>
+    <div class="col-6">
+
+        <div class="">
+            <img id="loginLogo" src="assets/Blue_Modern_QRCode.png" alt="trackQR_Logo">
+        </div>
+            <span class="welcomeFont">Welcome Back</span> -->
+        
+        <!-- <div class="row">
+            <input type="text" id="username" placeholder="Username">
+        </div>
+        <div class="row">
+            <input type="text" id="password" placeholder="Password">    
+        </div>
+        <div class="row">
+            <span id="forgotpassword" >Forgot password</span> 
+        </div>
+        <div class="row">
+            <span class="submit-btn">Login</span>
+        </div> -->
+           
+</div>
+  </div>
+</div>
 
 <!-- SCRIPTS -->
 <!-- <script src="<?= base_url('js/jquery/dist/jquery.min.js') ?>"></script> -->
