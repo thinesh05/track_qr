e9297e4b4ec67eaba73124c0e2e8372ca0407602
Thinesh <thineshtj05@gmail.com>
2023-06-01 15:13:51 +0800

    login page draft

--- app/Views/Login.php
+++ app/Views/Login.php
@@ -6,156 +6,72 @@
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;0,900;1,500;1,600;1,700;1,900&display=swap" rel="stylesheet">
     <!-- STYLES -->
 
     <style {csp-style-nonce}>
         html, body {
             color: rgba(33, 37, 41, 1);
-            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+            font-family: "Poppins", sans-serif;
             font-size: 16px;
             margin: 0;
-            padding: 10px;
             -webkit-font-smoothing: antialiased;
             -moz-osx-font-smoothing: grayscale;
             text-rendering: optimizeLegibility;
+            width: 100%;
+            height: 100%;
         }
-
-        .welcomeFont {
-            font-weight:700;
-            color : #4495FA;
-            font-size:36px;
+        .main-container {
+            display: flex;
+            flex-direction: row;
+            height: 100%
         }
-
-        .submit-btn {
-            border: 2px solid #4495FA;
-            color :#4495FA;
-            padding: 10px; 
-            cursor: pointer;
-            border-radius : 36px;
-            /* padding-right: 10%;
-            padding-left:10%; */
-            width : 100%;
-            font-weight : 700;
-            margin-top :15px;
-
+        .container-section-left,
+        .container-section-right {
+            width: 50%;
         }
-        .submit-btn:hover {
-            color: #fff;
-            background: #4495FA;
-            border-radius : 36px;
-            width : 100%;
+        .container-section-right {
+            color: #4495FA;
+            font-weight: 600;
+            font-size: 32px;
         }
-
-        #username,#password {
-            border: 1px solid transparent;
-            padding: 10px;
-            border-radius : 36px;
+        .container-section-right .login-input {
+            display: flex;
+            flex-direction: column;
+            gap: 18px;
+        }
+        .container-section-right input[type="text"],
+        .container-section-right input[type="password"] {
+            padding: 14px 16px;
+            border-radius: 24px;
+            border: 0px;
             background: #F8F8F8;
-            width : 100%;
-            font-weight : 100;         
-            margin-top : 15px;   
         }
-
-        * {
-  box-sizing: border-box;
-}
-
-
-/* style inputs and link buttons */
-input,
-.btn {
-  width: 100%;
-  padding: 12px;
-  border: none;
-  border-radius: 36px;
-  background: #F8F8F8;
-  margin: 5px 0;
-  opacity: 0.85;
-  display: inline-block;
-  font-size: 17px;
-  line-height: 20px;
-  text-decoration: none; /* remove underline from anchors */
-}
-
-input:hover,
-.btn:hover {
-  opacity: 1;
-}
-
-
-/* Two-column layout */
-.col {
-  float: left;
-  width: 50%;
-  margin: auto;
-  padding: 0 50px;
-  margin-top: 6px;
-}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-    
-
+        .square {
+            background: blue;
+            width: 100%;
+            height: 100%;
+        }
     </style>
 </head>
-
 <body>
-
-
-<div class="container">
-  <form action="/action_page.php">
-    <div class="row">
-
-      <div class="col">
-        <img id="welcomeImg" src="assets/welcomePage.png" alt="Welcome">
-      </div>
-
-      <div class="col">
-        <img id="loginLogo" src="assets/Blue_Modern_QRCode.png" alt="trackQR_Logo">
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <input class="submit-btn" type="submit" value="Login">
-      </div>
-      
+    <div class="main-container">
+        <div class="container-section-left">
+            <div class="square"></div>
+        </div>
+        <div class="container-section-right">
+            <span class="welcome-back">Welcome Back</span>
+            <div class="login-input">
+                <input type="text" name="username" id="username" placeholder="Username">
+                <input type="password" name="password" id="password" placeholder="Password">
+            </div>
+            <span class="login-btn">Login</span>
+        </div>
     </div>
-  </form>
-</div>
-
-
-<!-- <div class="container">
-  <div class="row-12 flex">
-    <div class="col-6">
-        <img id="welcomeImg" src="assets/welcomePage.png" alt="Welcome">
-    </div>
-    <div class="col-6">
-
-        <div class="">
-            <img id="loginLogo" src="assets/Blue_Modern_QRCode.png" alt="trackQR_Logo">
-        </div>
-            <span class="welcomeFont">Welcome Back</span> -->
-        
-        <!-- <div class="row">
-            <input type="text" id="username" placeholder="Username">
-        </div>
-        <div class="row">
-            <input type="text" id="password" placeholder="Password">    
-        </div>
-        <div class="row">
-            <span id="forgotpassword" >Forgot password</span> 
-        </div>
-        <div class="row">
-            <span class="submit-btn">Login</span>
-        </div> -->
-           
-</div>
-  </div>
-</div>
-
+font x change pula
+jap need to check
 <!-- SCRIPTS -->
 <!-- <script src="<?= base_url('js/jquery/dist/jquery.min.js') ?>"></script> -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
