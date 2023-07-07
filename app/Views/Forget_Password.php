458e2331329eab8a8b485ceedca1daf321f129e4
Thinesh <thineshtj05@gmail.com>
2023-07-07 13:41:26 +0800

    change icon

--- app/Views/Forget_Password.php
+++ app/Views/Forget_Password.php
@@ -5,7 +5,7 @@
     <title>Track QR</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+    <link rel="shortcut icon" type="image/png" href="/logo.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/stylesheet.css'); ?>">
